69314012f8690aaa1d118e6cb587730dd5e438d5
MonikaMohapatra <monikagudi123@gmail.com>
2026-01-20 18:56:38 +0530

    new change

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <footer className="pl-footer">
       <div className="pl-footer-inner">
+
         {/* LEFT COLUMN */}
         <div className="pl-footer-left">
           <p className="copyright">
@@ -24,10 +25,18 @@ const Footer = () => {
           <div className="regions">US&nbsp;&nbsp;UK&nbsp;&nbsp;JP&nbsp;&nbsp;KR</div>
 
           <div className="social-buttons">
-            <button>YOUTUBE</button>
-            <button>X</button>
-            <button>LINKEDIN</button>
-            <button>GITHUB</button>
+            <a href="https://youtube.com" target="_blank" rel="noreferrer">
+              <button>YOUTUBE</button>
+            </a>
+            <a href="https://x.com" target="_blank" rel="noreferrer">
+              <button>X</button>
+            </a>
+            <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+              <button>LINKEDIN</button>
+            </a>
+            <a href="https://github.com" target="_blank" rel="noreferrer">
+              <button>GITHUB</button>
+            </a>
             <button>STORE</button>
           </div>
         </div>
@@ -35,60 +44,61 @@ const Footer = () => {
         {/* OFFERINGS */}
         <div className="pl-footer-col">
           <h5>OFFERINGS</h5>
-          <a>American Tech Fellowship</a>
-          <a>Anti-Money Laundering</a>
-          <a>Automotive & Mobility</a>
-          <a>Data Protection</a>
-          <a>Defense</a>
-          <a>Energy</a>
-          <a>Federal Health</a>
-          <a>Financial Services</a>
-          <a>Food & Beverage</a>
-          <a>Hospital Operations</a>
-          <a>Insurance</a>
-          <a>Life Sciences</a>
+          <button className="footer-btn">American Tech Fellowship</button>
+          <button className="footer-btn">Anti-Money Laundering</button>
+          <button className="footer-btn">Automotive & Mobility</button>
+          <button className="footer-btn">Data Protection</button>
+          <button className="footer-btn">Defense</button>
+          <button className="footer-btn">Energy</button>
+          <button className="footer-btn">Federal Health</button>
+          <button className="footer-btn">Financial Services</button>
+          <button className="footer-btn">Food & Beverage</button>
+          <button className="footer-btn">Hospital Operations</button>
+          <button className="footer-btn">Insurance</button>
+          <button className="footer-btn">Life Sciences</button>
         </div>
 
         {/* IMPACT STUDIES */}
         <div className="pl-footer-col">
           <h5>IMPACT STUDIES</h5>
-          <a>Airbus</a>
-          <a>Cleveland Clinic</a>
-          <a>Concordance</a>
-          <a>Doosan Infracore</a>
-          <a>Fujitsu</a>
-          <a>Jacobs</a>
-          <a>NHS</a>
-          <a>Pacific Gas & Electric</a>
-          <a>Swiss Re</a>
-          <a>World Food Programme</a>
+          <button className="footer-btn">Airbus</button>
+          <button className="footer-btn">Cleveland Clinic</button>
+          <button className="footer-btn">Concordance</button>
+          <button className="footer-btn">Doosan Infracore</button>
+          <button className="footer-btn">Fujitsu</button>
+          <button className="footer-btn">Jacobs</button>
+          <button className="footer-btn">NHS</button>
+          <button className="footer-btn">Pacific Gas & Electric</button>
+          <button className="footer-btn">Swiss Re</button>
+          <button className="footer-btn">World Food Programme</button>
         </div>
 
         {/* CAPABILITIES */}
         <div className="pl-footer-col">
           <h5>CAPABILITIES</h5>
-          <a>AI + ML</a>
-          <a>Data Integration</a>
-          <a>Digital Twin</a>
-          <a>Edge AI</a>
-          <a>Marketplace</a>
-          <a>Pipeline Builder</a>
-          <a>Process Mining</a>
-          <a>Real-Time Alerting</a>
-          <a>Streaming</a>
-          <a>Titanium</a>
+          <button className="footer-btn">AI + ML</button>
+          <button className="footer-btn">Data Integration</button>
+          <button className="footer-btn">Digital Twin</button>
+          <button className="footer-btn">Edge AI</button>
+          <button className="footer-btn">Marketplace</button>
+          <button className="footer-btn">Pipeline Builder</button>
+          <button className="footer-btn">Process Mining</button>
+          <button className="footer-btn">Real-Time Alerting</button>
+          <button className="footer-btn">Streaming</button>
+          <button className="footer-btn">Titanium</button>
         </div>
 
         {/* DOCUMENTS */}
         <div className="pl-footer-col">
           <h5>DOCUMENTS</h5>
-          <a>Developer Community</a>
-          <a>Platform Documentation</a>
-          <a>Trust Center</a>
-          <a>Cookies</a>
-          <a>Privacy Statement</a>
-          <a>Terms of Use</a>
+          <a href="/docs">Developer Community</a>
+          <a href="/docs">Platform Documentation</a>
+          <a href="/trust">Trust Center</a>
+          <a href="/cookies">Cookies</a>
+          <a href="/privacy">Privacy Statement</a>
+          <a href="/terms">Terms of Use</a>
         </div>
+
       </div>
     </footer>
   );

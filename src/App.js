a24d89e08a265d4a147cf5e15eeb51847b7d9558
MonikaMohapatra <monikagudi123@gmail.com>
2026-02-07 13:40:38 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,54 +1,58 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import NavBar from "./pages/NavBar/NavBar";
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
 import Footer from "./pages/Footer/Footer";
-import Admin from "./pages/Admin/Admin";
-import AdminPages from "./pages/AdminPages/AdminPages";
-import AddPage from "./pages/AddPage/AddPage";
-import EditPage from "./pages/EditPage/EditPage";
-import PageDetails from "./pages/PageDetails/PageDetails";
-import ServicePage from "./pages/ServicePage/ServicePage";
-import Blog from "./pages/Blog/Blog";
-import CaseStudies from "./pages/CaseStudies/CaseStudies";
-import ProjectsPage from "./pages/ProjectPage/ProjectPage";
-import IndustriesPage from "./pages/IndustriesPage/IndustriesPage";
-import TechnologyPage from "./pages/TechnologyPage/TechnologyPage";
+import Loader from "./components/Loader/Loader";
 
+// Lazy imports
+const Home = lazy(() => import("./pages/Home/Home"));
+const About = lazy(() => import("./pages/About/About"));
+const PageDetails = lazy(() => import("./pages/PageDetails/PageDetails"));
+const ServicePage = lazy(() => import("./pages/ServicePage/ServicePage"));
+const ProjectsPage = lazy(() => import("./pages/ProjectPage/ProjectPage"));
+const IndustriesPage = lazy(() => import("./pages/IndustriesPage/IndustriesPage"));
+const TechnologyPage = lazy(() => import("./pages/TechnologyPage/TechnologyPage"));
+const Blog = lazy(() => import("./pages/Blog/Blog"));
+const CaseStudies = lazy(() => import("./pages/CaseStudies/CaseStudies"));
+
+// Admin
+const Admin = lazy(() => import("./pages/Admin/Admin"));
+const AdminPages = lazy(() => import("./pages/AdminPages/AdminPages"));
+const AddPage = lazy(() => import("./pages/AddPage/AddPage"));
+const EditPage = lazy(() => import("./pages/EditPage/EditPage"));
 
 const App = () => {
   const location = useLocation();
-
-  // hide navbar/footer for admin routes
   const isAdminRoute = location.pathname.startsWith("/admin");
 
   return (
     <>
       {!isAdminRoute && <NavBar />}
 
-    <Routes>
-  {/* Public Routes */}
-  <Route path="/" element={<Home />} />
-  <Route path="/about" element={<About />} />
-  <Route path="/:pageType/:category" element={<PageDetails />} />
+      {/* 🔥 Loader shows here */}
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/:pageType/:category" element={<PageDetails />} />
 
-  <Route path="/services" element={<ServicePage />} />
-  <Route path="/projects" element={<ProjectsPage />} />
-  <Route path="/industries" element={<IndustriesPage />} />
-  <Route path="/technologies" element={<TechnologyPage />} />
+          <Route path="/services" element={<ServicePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/industries" element={<IndustriesPage />} />
+          <Route path="/technologies" element={<TechnologyPage />} />
 
-  <Route path="/blog" element={<Blog />} />
-  <Route path="/case-studies" element={<CaseStudies />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/case-studies" element={<CaseStudies />} />
 
-  {/* Admin Routes */}
-  <Route path="/admin" element={<Admin />}>
-    <Route path="pages" element={<AdminPages />} />
-    <Route path="add" element={<AddPage />} />
-    <Route path="edit/:id" element={<EditPage />} />
-  </Route>
-</Routes>
-
+          {/* Admin Routes */}
+          <Route path="/admin" element={<Admin />}>
+            <Route path="pages" element={<AdminPages />} />
+            <Route path="add" element={<AddPage />} />
+            <Route path="edit/:id" element={<EditPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
 
       {!isAdminRoute && <Footer />}
     </>
@@ -56,5 +60,3 @@ const App = () => {
 };
 
 export default App;
-
-
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="loader-wrapper">
+      <img src="/logo.png" alt="Loading" className="loader-logo" />
+    </div>
+  );
+};
+
+export default Loader;

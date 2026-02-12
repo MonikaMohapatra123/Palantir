05575e8f88c3d1799f9d857294dd46ae000bb513
MonikaMohapatra <monikagudi123@gmail.com>
2026-02-12 14:00:04 +0530

    new

--- src/components/PartnerCards/PartnerCards.jsx
+++ src/components/PartnerCards/PartnerCards.jsx
@@ -2,26 +2,31 @@ import React from "react";
 import "./PartnerCards.css";
 
 const PartnerCards = ({ partnerData }) => {
-  if (!partnerData) return null;
+  if (!partnerData || !partnerData.cards) return null;
+
+  const duplicatedCards = [...partnerData.cards, ...partnerData.cards];
 
   return (
     <section className="partner-section">
       <h2 className="partner-heading">
-        {partnerData.heading.line1}
+        {partnerData.heading?.line1}
       </h2>
 
       <div className="partner-scroll">
         <div className="partner-track">
-          {[...partnerData.cards, ...partnerData.cards].map((item, index) => (
-            <div className="partner-card" key={index}>
+          {duplicatedCards.map((item, index) => (
+            <div className="partner-card" key={`${item.name}-${index}`}>
               
-              {/* NAME */}
               <h4 className="card-title">{item.name}</h4>
 
-              {/* BODY */}
-              <div className="card-body">
-                <p className="card-description">{item.description}</p>
-                <p className="card-designation">{item.designation}</p>
+              <div className="card-content">
+                <p className="card-description">
+                  {item.description}
+                </p>
+
+                <p className="card-designation">
+                  {item.designation}
+                </p>
               </div>
 
             </div>
